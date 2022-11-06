c9d023b369a09b002f4eb40b40bce6ca1b412f75
wey1998 <tamyitwey@hotmail.com>
2022-11-07 02:55:13 +0800

    Added Milestone 4 Task 2

--- milestone_4.py
+++ milestone_4.py
@@ -6,7 +6,28 @@ class Hangman:
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(self.word_list)
-        self.word_guessed = ["_"] *len(word)
-        self.num_letters = len(set(word))
+        self.word_guessed = ["_"] *len(self.word)
+        self.num_letters = len(set(self.word))
         self.list_of_guesses = []
     
+    def check_guess(self,guess):
+        guess = guess.lower()
+        
+        if guess in set(self.word):
+            return "Good guess! {guess} is in the word"
+        
+    def ask_for_input(self):
+        while True:
+            guess = input("Enter a letter to guess the fruit: ")
+            
+            if len(guess) != 1:
+                print("Please only enter a single alphabetical character")
+            elif guess in self.list_of_guesses:
+                print(f"{guess} has already been tried")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+a = Hangman(["Apple"])
+
+a.ask_for_input()
